61af5fe214fdcaed35d6ecebf85d87c177976401
QiRenNP <104065272+QiRenNP@users.noreply.github.com>
2022-04-21 00:03:33 +0800

    Update Question4.java

--- Question4.java
+++ Question4.java
@@ -5,19 +5,24 @@ public class Question4
   public static void main(String[] args)
   {
     /**
-     * Prints a right angle triangle with *. The base of the triangle depends on the integer given.
-     *    e.g.
-     *     > 5
-     *     *****
-     *     ****
-     *     ***
-     *     **
-     *     *
+     * Reads integer entered by the user, multiplies the integer by itself and print out the result.
      * 
-     * Hint: 2 loops are required. System.out.print(...) to print on single line.
+     * Hint: in.nextInt() to read integer
      */
      
     Scanner in = new Scanner(System.in);
-    
+    int num = in.nextInt();
+    while (num!=0){
+      int count = 0;
+      while (true){
+        System.out.print("*");
+        count++;
+        if (count==num){
+          break;
+        }
+      }
+      System.out.println();
+      num = num-1;
+    }
   }
 }
