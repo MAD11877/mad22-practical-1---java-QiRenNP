962f01d671d1bddef490a7d3171fdd6d81f01229
QiRenNP <104065272+QiRenNP@users.noreply.github.com>
2022-04-21 00:05:49 +0800

    Update Question4.java

--- Question4.java
+++ Question4.java
@@ -4,11 +4,7 @@ public class Question4
 {
   public static void main(String[] args)
   {
-    /**
-     * Reads integer entered by the user, multiplies the integer by itself and print out the result.
-     * 
-     * Hint: in.nextInt() to read integer
-     */
+
      
     Scanner in = new Scanner(System.in);
     int num = in.nextInt();
